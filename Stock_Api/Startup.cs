634163721bf15d21636542c8e0bb4a2cca1ceb59
reinaldo <reinaldocostamendes3@gmail.com>
2022-06-22 10:05:06 +0100

    new update 01

--- Stock_Api/Startup.cs
+++ Stock_Api/Startup.cs
@@ -56,7 +56,7 @@ namespace Stock_Api
 
             IMapper mapper = mapperConfig.CreateMapper();
             services.AddSingleton(mapper);
-            // services.AddHostedService<ConsumerMessageRabbitMQStock>();
+            services.AddHostedService<ConsumerMessageRabbitMQStock>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
