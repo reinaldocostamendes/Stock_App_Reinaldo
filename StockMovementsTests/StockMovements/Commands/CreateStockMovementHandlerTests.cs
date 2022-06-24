dc8ac82919467fc18ffe86b1bdd31efbc26ad835
reinaldo <reinaldocostamendes3@gmail.com>
2022-06-24 14:19:59 +0100

    Testes criados

--- StockMovementsTests/StockMovements/Commands/CreateStockMovementHandlerTests.cs
+++ StockMovementsTests/StockMovements/Commands/CreateStockMovementHandlerTests.cs
@@ -60,7 +60,7 @@ namespace StockMovementsTests.StockMovements.Commands
                 PageIndex = 1,
                 PageSize = 5
             });
-            result.ShouldBe
+
             result.ShouldBeOfType<StockMovementDTO>();
         }
     }
