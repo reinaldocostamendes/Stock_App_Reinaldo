300bad97e26801db24901140e4c211030a02b56d
reinaldo <reinaldocostamendes3@gmail.com>
2022-06-24 14:19:35 +0100

    Testes criados

---
+++ StockMovementsTests/StockCommandFaker.cs
@@ -0,0 +1,56 @@
+﻿using Bogus;
+using StockMovement_Application.Commands.Create.StockMoviment;
+using StockMovement_Data.Enums;
+using StockMovement_Domain.Enums;
+using StockMovement_Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMovementsTests
+{
+    public class StockCommandFaker
+    {
+        public static CreateStockMovementCommand Generate()
+        {
+            Faker<StockMovementProduct> prodReq = new Faker<StockMovementProduct>()
+               .RuleFor(x => x.Id, Guid.NewGuid())
+               .RuleFor(x => x.ProductId, Guid.NewGuid())
+               .RuleFor(x => x.ProductDescription, x => x.Random.String(1, 256))
+               .RuleFor(x => x.ProductType, x => x.PickRandom<ProductType>())
+               .RuleFor(x => x.Quantity, x => x.Random.Int(1, 10))
+               .RuleFor(x => x.UnitValue, x => x.Random.Decimal(1, 100));
+
+            CreateStockMovementCommand stockReq = new Faker<CreateStockMovementCommand>()
+
+                     .RuleFor(x => x.OriginId, x => Guid.NewGuid())
+                     .RuleFor(x => x.Origin, x => x.PickRandom<StockOrigin>())
+                     .RuleFor(x => x.Type, x => x.PickRandom<StockType>())
+                     .RuleFor(x => x.Date, x => DateTime.Now)
+                     .RuleFor(x => x.Products, x => prodReq.GenerateBetween(1, 5));
+            return stockReq;
+        }
+
+        public static CreateStockMovementCommand GenerateInvlaid()
+        {
+            Faker<StockMovementProduct> prodReq = new Faker<StockMovementProduct>()
+               .RuleFor(x => x.Id, Guid.NewGuid())
+               .RuleFor(x => x.ProductId, Guid.Empty)
+               .RuleFor(x => x.ProductDescription, x => x.Random.String(1, 256))
+               .RuleFor(x => x.ProductType, x => x.PickRandom<ProductType>())
+               .RuleFor(x => x.Quantity, x => x.Random.Int(1, 10))
+               .RuleFor(x => x.UnitValue, x => x.Random.Decimal(1, 100));
+
+            CreateStockMovementCommand stockReq = new Faker<CreateStockMovementCommand>()
+
+                     .RuleFor(x => x.OriginId, x => Guid.NewGuid())
+                     .RuleFor(x => x.Origin, x => x.PickRandom<StockOrigin>())
+                     .RuleFor(x => x.Type, x => x.PickRandom<StockType>())
+                     .RuleFor(x => x.Date, x => DateTime.Now)
+                     .RuleFor(x => x.Products, x => prodReq.GenerateBetween(1, 5));
+            return stockReq;
+        }
+    }
+}
---
+++ StockMovementsTests/StockMovements/Commands/CreateStockMovementHandlerTests.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using Moq;
+using Shouldly;
+using StockMovement_Application.Commands.Create.StockMoviment;
+using StockMovement_Application.Dtos;
+using StockMovement_Application.Mapping;
+using StockMovementData.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StockMovementsTests.StockMovements.Commands
+{
+    public class CreateStockMovementHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IStockMovementRepository> _mockUow;
+        private readonly CreateStockMovementHandler _handler;
+
+        public CreateStockMovementHandlerTests()
+        {
+            _mockUow = new Mock<IStockMovementRepository>();
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+
+            _handler = new CreateStockMovementHandler(_mapper, _mockUow.Object);
+        }
+
+        [Fact]
+        public async Task Valid_LeaveType_Added()
+        {
+            var command = StockCommandFaker.Generate();
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            var listStock = await _mockUow.Object.GetAllStockMovement(new Infrastructure.Entity.PageParameters()
+            {
+                PageIndex = 1,
+                PageSize = 5
+            });
+
+            result.ShouldBeOfType<StockMovementDTO>();
+        }
+
+        [Fact]
+        public async Task InValid_LeaveType_Added()
+        {
+            var command = StockCommandFaker.GenerateInvlaid();
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            var listStock = await _mockUow.Object.GetAllStockMovement(new Infrastructure.Entity.PageParameters()
+            {
+                PageIndex = 1,
+                PageSize = 5
+            });
+            result.ShouldBe
+            result.ShouldBeOfType<StockMovementDTO>();
+        }
+    }
+}
